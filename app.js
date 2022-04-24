4d57a233a6ce25881c4674e952209d67a8f0fc51
jmjusmin <jmjusmin1412@gmail.com>
2022-04-23 23:29:59 -0700

    finish up the project

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+var _ = require('lodash');
 
 //global variable
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -51,12 +52,16 @@ app.post("/compose", (req,res)=>{
 });
 
 app.get("/posts/:postName", (req,res)=>{
-  const reqTitle = req.params.postName;
+  const reqTitle = _.lowerCase(req.params.postName);
 
   posts.forEach(function(post){
-    const storeTitle = post.title;
+    const storeTitle = _.lowerCase(post.title);
+
     if(storeTitle === reqTitle){
-      console.log("Match found!");
+      res.render("post",{
+        title: post.title,
+        content: post.content
+      });
     }
   });
   
